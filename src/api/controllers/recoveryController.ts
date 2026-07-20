56752f87e720bb39e47af4757131caf330ea3cae
hm0416 <70279064+hm0416@users.noreply.github.com>
2026-07-19 23:46:35 -0500

    added getVideo for client + controller + service

---
+++ src/api/controllers/recoveryController.ts
@@ -0,0 +1,34 @@
+import { getVideoByTopic, getVideos, initDatabase } from '../services/recoveryService';
+import { QuestionKey } from '../types';
+
+export async function loadVideosController() {
+    await initDatabase(); // initializes DB before getting videos, double check this 
+    return getVideos();
+}
+
+export async function getVideoBasedOnScoreController(scores: Record<QuestionKey, number | null>) {
+    await initDatabase(); // ensures DB is initialized before fetching videos
+    // logic to select video based on score
+    // for real use can use the DASS-21 (Depression, Anxiety, and Stress Scales) to determine which videos 
+
+    // made up calculation for sake of excerise 
+    // if stress anixety depression >= 3 ---> pick depression video 
+    // if stress >= 3 and anxiety >= 3 -> pick stress video 
+    // if anxiety >= 3 -> pick anxiety video 
+
+    // using if because evaluating bools, switch good for comparing one val against other cases 
+    if (scores.stress !== null && scores.anxiety !== null && scores.depression !== null) {
+        if (scores.stress >= 3 && scores.anxiety >= 3 && scores.depression >= 3) {
+            return await getVideoByTopic('depression');
+        } else if (scores.stress >= 3 && scores.anxiety >= 3 && scores.depression < 3) {
+            return await getVideoByTopic('stress');
+        } else if (scores.anxiety >= 3 && scores.stress < 3 && scores.depression < 3) {
+            return await getVideoByTopic('anxiety');
+        } else if (scores.stress < 3 && scores.anxiety < 3 && scores.depression >= 3) {
+            return await getVideoByTopic('depression');
+        } else if (scores.stress >= 3 && scores.anxiety < 3 && scores.depression < 3) {
+            return await getVideoByTopic('stress');
+        }
+    }
+    return await getVideoByTopic('anxiety'); // default video if no conditions match
+}
